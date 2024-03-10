8d2aaca90f161da8b115616cb6485e0d6cf5394b
Ravi Patel <ravifromchanga@gmail.com>
2024-03-10 10:46:26 +0530

    banner fonts updated

--- components/section/banner-5.jsx
+++ components/section/banner-5.jsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 // import { useEffect, useState } from 'react';
-import Head from 'next/head';
+// import Head from "next/head";
 
 // Import your styles
-import 'normalize.css/normalize.css';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
-import '@/css/slider-animations.css';
-import '@/css/slider-banner.css';
+import "normalize.css/normalize.css";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "@/css/slider-animations.css";
+import "@/css/slider-banner.css";
 
 // Your component content remains the same
 
@@ -23,83 +23,83 @@ const BannerFive = () => {
       userProfile: "https://i.imgur.com/JSW6mEk.png",
     },
 
-  {
-    id: "2",
-    title: "Laboratories",
-    description:
-      "Highly equipped and advanced labs for practical learning and in-depth research",
-    button: "Apply Now",
-    image: "assets/images/home/lab.webp",
-    user: "Erich Behrens",
-    userProfile: "https://i.imgur.com/0Clfnu7.png",
-  },
-  {
-    id: "3",
-    title: "Hostels",
-    description:
-      "Our hostels give out the vibes of comfort and contentment, just like home",
-    button: "Apply Now",
-    image: "assets/images/home/s3.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },
-  {
-    id: "4",
-    title: "Wi Fi Campus",
-    description:
-      "Connect with the world on CHARUSAT that is entirely wi-fi enabled",
-    button: "Apply Now",
-    image: "assets/images/home/s4.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },
-  {
-    id: "5",
-    title: "Canteen",
-    description:
-      "Wholesome meals served as a part of indian and world cuisines replete with all 5 flavours",
-    button: "Apply Now",
-    image: "assets/images/home/canteen.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },
-  {
-    id: "6",
-    title: "Infrastucture",
-    description:
-      "Future Ready Academics infrastructure with ergonomically designed classrooms, ICT and climatization",
-    button: "Apply Now",
-    image: "assets/images/home/s7.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },
-  {
-    id: "7",
-    title: "ATHLETICS & FITNESS",
-    description:
-      "Physical benefits of recreational sports on the sports fields and in gymnasium facilitates. Indoor sports facility for mental endurance and relaxation",
-    button: "Apply Now",
-    image: "assets/images/home/s8.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },
-  {
-    id: "8",
-    title: "Green Campus",
-    description: "Creating a Sustainable and Eco-Friendly Campus Environment",
-    button: "Apply Now",
-    image: "assets/images/home/s9.webp",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
-  },]
+    {
+      id: "2",
+      title: "Laboratories",
+      description:
+        "Highly equipped and advanced labs for practical learning and in-depth research",
+      button: "Apply Now",
+      image: "assets/images/home/lab.webp",
+      user: "Erich Behrens",
+      userProfile: "https://i.imgur.com/0Clfnu7.png",
+    },
+    {
+      id: "3",
+      title: "Hostels",
+      description:
+        "Our hostels give out the vibes of comfort and contentment, just like home",
+      button: "Apply Now",
+      image: "assets/images/home/s3.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+    {
+      id: "4",
+      title: "Wi Fi Campus",
+      description:
+        "Connect with the world on CHARUSAT that is entirely wi-fi enabled",
+      button: "Apply Now",
+      image: "assets/images/home/s4.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+    {
+      id: "5",
+      title: "Canteen",
+      description:
+        "Wholesome meals served as a part of indian and world cuisines replete with all 5 flavours",
+      button: "Apply Now",
+      image: "assets/images/home/canteen.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+    {
+      id: "6",
+      title: "Infrastucture",
+      description:
+        "Future Ready Academics infrastructure with ergonomically designed classrooms, ICT and climatization",
+      button: "Apply Now",
+      image: "assets/images/home/s7.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+    {
+      id: "7",
+      title: "ATHLETICS & FITNESS",
+      description:
+        "Physical benefits of recreational sports on the sports fields and in gymnasium facilitates. Indoor sports facility for mental endurance and relaxation",
+      button: "Apply Now",
+      image: "assets/images/home/s8.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+    {
+      id: "8",
+      title: "Green Campus",
+      description: "Creating a Sustainable and Eco-Friendly Campus Environment",
+      button: "Apply Now",
+      image: "assets/images/home/s9.webp",
+      user: "Bruno Vizovskyy",
+      userProfile: "https://i.imgur.com/4KeKvtH.png",
+    },
+  ];
 
   return (
     <section className="d-block clearfix">
-      
-      <Head>
+      {/* <Head>
         <title>Your Page Title</title>
  
-      </Head>
+      </Head> */}
 
       <Slider autoplay="4000" className="slider-wrapper">
         {content.map((item, index) => (
@@ -109,18 +109,20 @@ const BannerFive = () => {
             style={{
               background: `url('${item.image}') no-repeat center center`,
             }}
+            width="100%"
+            height="100%"
           >
             <div className="inner">
               <div
                 className="main-text-slider animated-text-front-page"
                 style={{
-                  fontFamily: 'Playfair Display',
-                  fontWeight: '700', 
+                  // fontFamily: "Playfair Display",
+                  fontWeight: "700",
                 }}
               >
                 <h1> {item.title}</h1>
               </div>
-              <p className="subTitle-slider text_1" style={{ color: 'black' }}>
+              <p className="subTitle-slider text_1" style={{ color: "black" }}>
                 {item.description}
               </p>
             </div>
@@ -132,8 +134,5 @@ const BannerFive = () => {
     </section>
   );
 };
-
-
-
 
 export default BannerFive;
