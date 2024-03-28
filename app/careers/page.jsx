60cd65f71d41b5489ffe30d214e634e5c37d3d50
Ravi Patel <ravifromchanga@gmail.com>
2024-03-28 11:08:02 +0530

    Career Page Updated

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -1,33 +1,34 @@
-"use client"
+"use client";
 import { Fragment } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import img1 from "@/public/assets/images/advertisment_nov_2022.webp"
-import Image from "next/image"
+// import img1 from "@/public/assets/images/advertisment_nov_2022.webp"
+import Image from "next/image";
 // import { Link } from "next/link";
 //import PageHeader from "../component/layout/pageheader";
 // import career1_img from "../assets/images/advertisment_nov_2022.webp";
 
-
 const Certification_Courses = () => {
   return (
     <Fragment>
-      
-        <title>Careers @ CHARUSAT</title>
-        <meta httpEquiv="Content-Type" content=" text/html; charset=utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Career " />
-        <meta name="keywords" content="" />
-        <meta name="robots" content="index" />
-        <meta name="robots" content="follow" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="google" content="translate" />
-        <meta name="google" content="nopagereadaloud" />
-        <meta property="og:type" content="homepage" />
-        <meta property="og:title" content="CSPIT Homepage" />
-        <meta property="og:description" content="CSPIT - best engineering college to study in Gujarat." />
-        <meta name="author" content="CHARUSAT Web Team" />
-      
+      <title>Careers @ CHARUSAT</title>
+      <meta httpEquiv="Content-Type" content=" text/html; charset=utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Career " />
+      <meta name="keywords" content="" />
+      <meta name="robots" content="index" />
+      <meta name="robots" content="follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="google" content="translate" />
+      <meta name="google" content="nopagereadaloud" />
+      <meta property="og:type" content="homepage" />
+      <meta property="og:title" content="CSPIT Homepage" />
+      <meta
+        property="og:description"
+        content="CSPIT - best engineering college to study in Gujarat."
+      />
+      <meta name="author" content="CHARUSAT Web Team" />
+
       {/* <PageHeader title={'4 Results found for: Business'} curPage={'Search Result'} /> */}
       <div className="blog-section padding-tb section-bg">
         <div className="container">
@@ -46,27 +47,35 @@ const Certification_Courses = () => {
                             <Container>
                               <Row className="p-2">
                                 <Col md={8}>
-                                  <Image
-                                    className="fluid"
-                                    src={img1}
-                                    alt={"career"}
-                                    
-                                  />
-                                </Col>
-                                <Col md={4}>
-                                  <h6 className="fw-bold">Recruitment - Various positions</h6>
-                                  
+                                  <h6 className="fw-bold">
+                                    Recruitment - Various positions
+                                  </h6>
 
                                   <a
                                     className="lab-btn text-white mt-2"
                                     style={{
-                                      backgroundColor:"#0066b3"
+                                      backgroundColor: "#0066b3",
                                     }}
                                     href="https://charusat.edu.in:912/OnlineJobPortal/"
                                   >
                                     Apply Now
                                   </a>
                                 </Col>
+                                {/* <Col md={4}>
+                                  <h6 className="fw-bold">
+                                    Recruitment - Various positions
+                                  </h6>
+
+                                  <a
+                                    className="lab-btn text-white mt-2"
+                                    style={{
+                                      backgroundColor: "#0066b3",
+                                    }}
+                                    href="https://charusat.edu.in:912/OnlineJobPortal/"
+                                  >
+                                    Apply Now
+                                  </a>
+                                </Col> */}
                               </Row>
                             </Container>
                             <br />
