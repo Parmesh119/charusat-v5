e477738603087fd76d6b4943a80a57ffc1d16d32
itdepartment2018 <hod.it@charusat.ac.in>
2024-04-02 12:39:26 +0530

    programmes updated

--- components/section/Navigation.jsx
+++ components/section/Navigation.jsx
@@ -95,7 +95,7 @@ const Navigation = () => {
                     activeLink === "Academics" ? "green-text" : "green"
                   }`}
                 >
-                  Programs
+                  Programmes
                 </Link>
               </li>
 
